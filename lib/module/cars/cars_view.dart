92883c9f2734f6f14d1752b59983a7ff1d930950
ilhamandri <ilhamandriandri@gmail.com>
2022-06-23 14:00:44 +0700

    create folders

---
+++ lib/module/cars/cars_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CarsView extends StatelessWidget {
+  const CarsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
